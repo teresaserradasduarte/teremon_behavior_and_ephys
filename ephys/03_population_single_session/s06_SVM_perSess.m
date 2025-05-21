804a611236c7a281bc03448d4a3ed807fd146922
teresaserradasduarte <teresa.serradasduarte@neuro.fchampalimaud.org>
2025-05-21 15:08:13 +0100

    update SVM

--- ephys/03_population_single_session/s06_SVM_perSess.m
+++ ephys/03_population_single_session/s06_SVM_perSess.m
@@ -15,29 +15,44 @@ animals = {...
 
 all_sess = {['R1';'R6'],...
     ['R1';'R2';'R4'],...
-    ['R3';'R6'],...
+    ['R3'],...
     ['R4'],...
     ['R1';'R4';'R6']};
 
-reg = 'CB';
+reg = 'BG';
 
 % Initialize
 SVM_sess = struct();
-load(fullfile(rootdir,"behavior_data/","analyzed_data/mat_files/","20230511_ChocolateGroup/","headfixed_dynamicTarget/","1_CoteDor/","R6/","behavior_session.mat"))
-colors = behavior.colors;
-clear behavior;
+cc = 0; % counter of sessions (all mice)
 
 % Save mat dir
 save_mat = fullfile(rootdir,"ephys_and_behavior","mat_files",group);
 
+%% Window
+win_int = [-2.5 2.5];
+tm_before = win_int(1);
+tm_after = win_int(2); % sec
+win_dur = tm_after-tm_before;
+bin_width = 0.1;
+bin_step = 0.025;
+n_bins = floor((win_dur/bin_step)-(bin_width/bin_step))+1;
+
+%% Figure defs
+axeOpt = {'linewidth',1.5,'box','off','GridAlpha',...
+    0.05,'ticklength',[1,1]*.01,'fontsize',10, 'TickDir', 'out'};
+clr_center = [72 60 50]./256;
+clr_dom = [48 131 220]./256;
+clr_nondom = [158 216 219]./256;
+clr_init = [63 130 109]./256;
+clrs = [clr_init;clr_dom;clr_center;clr_nondom];
 
 %% SVM - Lood through mice and session
-for m=1%1:length(animals)
+for m=1:length(animals)
     animal_idx = m;
     mouse = sprintf('%i_%s',animal_idx,animals{animal_idx});
     fprintf('%s%s%s\n','Looping through ',mouse,':')
 
-    for s=1:2%:size(all_sess{m},1)
+    for s=1:size(all_sess{m},1)
         sess = all_sess{m}(s,:);
         fprintf('%s%s%s%s%s\n','Running mouse ',mouse,', session ',sess,':')
 
@@ -48,24 +63,16 @@ for m=1%1:length(animals)
 
             % Load mat files
             load(fullfile(ephys_struct_dir,['neu_',reg,'_struct.mat'])');
+            cc = cc+1;
 
             % SVM struct
-            SVM_sess(s).mouse = neu_strct.mouse;
-            SVM_sess(s).sess = neu_strct.sess;
+            SVM_sess(cc).mouse = neu_strct.mouse;
+            SVM_sess(cc).sess = neu_strct.sess;
 
             % Number of neurons
             n_neu = length(neu_strct);
-            SVM_sess(s).n_neu = n_neu;
+            SVM_sess(cc).n_neu = n_neu;
 
-
-            %% Window
-            win_int = [-2.5 2.5];
-            tm_before = win_int(1);
-            tm_after = win_int(2); % sec
-            win_dur = tm_after-tm_before;
-            bin_width = 0.1;
-            bin_step = 0.05;
-            n_bins = floor((win_dur/bin_step)-(bin_width/bin_step))+1;
 
             %%
             i_pp_trials_all = neu_strct.idx_init_vPP_invPP;
@@ -130,10 +137,8 @@ for m=1%1:length(animals)
 
             % Parameters
             ratio_train_val = 0.8;
-            %     ncv = 100;
-            %     nfold = 10;
-            ncv = 2;
-            nfold = 2;
+            ncv = 100;
+            nfold = 10;
             Cvec = [0.001, 0.01, 0.1, 1, 10, 100, 1000];
 
             % store svm classifier accuracy
@@ -188,23 +193,15 @@ for m=1%1:length(animals)
             toc
 
             % Save
-            SVM_sess(s).bac_init = bac_init;
-            SVM_sess(s).bac_reach = bac_reach;
+            SVM_sess(cc).bac_init = bac_init;
+            SVM_sess(cc).bac_reach = bac_reach;
 
             %% FIGURE
 
             %Save out dir
-            save_out_dir = fullfile(rootdir,"ephys_and_behavior/","mat_files/",group,mouse,sess,reg,'SVM');
+            save_out_dir = fullfile(rootdir,"ephys_and_behavior/","out_files/",group,mouse,sess,reg,'SVM');
             if ~exist(save_out_dir,"dir"), mkdir(save_out_dir); end
 
-            axeOpt = {'linewidth',1.5,'box','off','GridAlpha',...
-                0.05,'ticklength',[1,1]*.01,'fontsize',10, 'TickDir', 'out'};
-            clr_center = colors.center_color;
-            clr_dom = colors.clr_dom;
-            clr_nondom = colors.clr_nondom;
-            clr_init = [63 130 109]./256;
-
-            clrs = [clr_init;clr_dom;clr_center;clr_nondom];
             %%
             bin_mean=bin_edges(:,1)+diff(bin_edges(1:2));
 
@@ -216,25 +213,40 @@ for m=1%1:length(animals)
             xlabel('time from trial init (s)'); ylabel('accuracy');
             set(gca,axeOpt{:})
             yline(.5,'--','color',[.7 .7 .7 .7],'LineWidth',1)
-            xlim([win_int]); ylim([.45 1])
+            xlim(win_int); ylim([.45 1])
+            title(mouse,'Interpreter','none');
 
             subplot(122)
             for plt=1:4
                 plot(bin_mean,bac_reach(:,plt),'color',clrs(plt,:),'linewidth',2); hold on
             end
-            xlabel('time from trial init (s)'); ylabel('accuracy');
+            xlabel('time from reach endpoint (s)'); ylabel('accuracy');
             set(gca,axeOpt{:})
             yline(.5,'--','color',[.7 .7 .7 .7],'LineWidth',1)
-            xlim([win_int]); ylim([.45 1])
+            xlim(win_int); ylim([.45 1])
+            title(sess)
 
-            set(gcf,'Position',[2289         387        1347         465])
+            set(gcf,'Position',[680         585        1226         393],'Color','w');
+            saveas(gcf,strcat(save_out_dir,filesep,'svm_accuracy.png'),'png');
 
         end
     end
 end
+fprintf('Saving...\n')
 
+% Params
+SVM_sess(cc).params.win_int = win_int;
+SVM_sess(cc).params.ratio_train_val = ratio_train_val;
+SVM_sess(cc).params.ncv = ncv;
+SVM_sess(cc).params.nfold = nfold;
+SVM_sess(cc).params.Cvec = Cvec;
+SVM_sess(cc).params.bin_width = bin_width;
+SVM_sess(cc).params.bin_step = bin_step;
 % Save mat
-save(strcat(save_mat,filesep,'SVM_sess.mat'),"SVM_sess");
+save(strcat(save_mat,filesep,'SVM_sess_',reg,'.mat'),"SVM_sess");
+
+fprintf('Add done!!\n')
+
 
 
 
